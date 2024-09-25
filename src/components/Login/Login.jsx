286c978b2394b8aad7768e57d801a1a0b0911abf
subas-roy <subasroy46@gmail.com>
2024-09-26 05:02:33 +0600

    Login User and Accept terms and conditions

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,73 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+import { useState } from "react";
 
 const Login = () => {
+  const [registerError, setRegisterError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handleLogin = e => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    
+    // reset error
+    setRegisterError('')
+    setSuccess('')
+    
+    // add validation
+    
+    // sign in
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      result.user
+      setSuccess('User logged in successfully.')
+    })
+    .catch(error => {
+      console.error(error)
+      setRegisterError(error.message);
+    })
+  }
+
+
+
   return (
-    <div>
-      <h2 className="text-3xl">Please Login</h2>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
+              {/* <button type="submit" onClick={handleLogin} className="btn btn-primary">Login</button> */}
+            </div>
+            {registerError && <p className="text-red-700 mt-2">{registerError}</p>}
+            {success && <p className="text-green-700 mt-4">{success}</p>}
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -12,18 +12,23 @@ const Register = () => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password)
+    const accepted = e.target.terms.checked;
+    console.log(email, password, accepted)
 
     // reset error
     setRegisterError('')
     setSuccess('')
     
     if (password.length < 6) {
-      setRegisterError('Password should be at least 6 characters')
+      setRegisterError('Password should be at least 6 characters.')
       return; // ends the program
     }
     else if (!/[A-Z]/.test(password)) {
-      setRegisterError('Your password should have an uppercase character!')
+      setRegisterError('Your password should have an uppercase character.')
+      return;
+    }
+    else if (!accepted) {
+      setRegisterError('Please accpted our Terms and Conditions.')
       return;
     }
 
@@ -31,7 +36,7 @@ const Register = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         console.log(result.user)
-        setSuccess('User created successfully!')
+        setSuccess('User created successfully.')
       })
       .catch(error => {
         console.error(error);
@@ -43,18 +48,23 @@ const Register = () => {
       <div className="mx-auto md:w-1/2 ">
         <h2 className="text-3xl">Please Register</h2>
         <form onSubmit={handleRegister}>
-          <input name="email" type="email" required placeholder="Email Address" className="mb-4 w-3/4 px-4 py-2 border" />
+          <input name="email" type="email" required placeholder="Email Address" className="mb-4 w-full px-4 py-2 border" />
           <br />
-          <input name="password" 
-          type={showPassword ? "text" : "password"} 
-          required placeholder="Password" className="mb-4 w-3/4 px-4 py-2 border" />
-          <span onClick={() => setShowPassword(!showPassword)}>
-            {
-              showPassword ? <FaEyeSlash/> : <FaEye/>
-            }
-          </span>
-          <br />
-          <input type="submit" value="Register" className="w-3/4 btn btn-secondary" />
+          <div className="relative">
+            <input name="password" 
+            type={showPassword ? "text" : "password"} 
+            required placeholder="Password" className="mb-4 w-full border px-4 py-2" />
+            <span className="absolute top-3 right-2" onClick={() => setShowPassword(!showPassword)}>
+              {
+                showPassword ? <FaEyeSlash/> : <FaEye/>
+              }
+            </span>
+          </div>
+          <div className="mb-3">
+            <input type="checkbox" name="terms" id="terms" />
+            <label className="ml-2" htmlFor="terns">Accept our <a href="#">Terms and Conditions</a></label>
+          </div>
+          <input type="submit" value="Register" className="w-full btn btn-secondary" />
         </form>
         {registerError && <p className="text-red-700 mt-2">{registerError}</p>}
         {success && <p className="text-green-700 mt-4">{success}</p>}
